220200737aa86929a53ba7d35f15678dab26087a
Гошанский <mc.goshanskyi@yandex.ru>
2025-04-20 10:43:26 +0300

    Теперь в контейнере

--- user/core/config.py
+++ user/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
 
     POSTGRES_USER: str = os.getenv("POSTGRES_USER", "goyda_user")
     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "goyda_password")
-    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "db")
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", "5432")
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "goyda_db")
     DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
